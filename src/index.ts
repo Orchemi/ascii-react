59ccd6c35e0cba8d56c44cdf601a73f1527f3d0b
tmdgns1126 <tmdgns1126@naver.com>
2025-07-08 16:17:02 +0900

    feat: 상수, 타입 export

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
 export { default as AsciiMedia } from "./components/AsciiMedia";
+export type {
+  HexColor,
+  AsciiColor,
+  MediaType,
+  CharsRandomLevel,
+  CharList,
+  CharMatrix,
+  AsciiMediaProps,
+  ManualCharColor,
+} from "./components/asciiMedia.type";
+export {
+  defaultCharList,
+  defaultCharMatrix,
+  defaultBackgroundColor,
+} from "./components/asciiMedia.constant";
